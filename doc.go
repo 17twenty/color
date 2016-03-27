583ece7bc7d35a32e3df3fce8aa43eb0159a40ef
Anmol Sethi <anmol@aubble.com>
2016-03-27 17:51:20 -0400

    remove examples from godoc

--- doc.go
+++ doc.go
@@ -79,55 +79,5 @@
 		%h[fgBlue+bgRed+bold]
 
 	See http://goo.gl/LRLA7o for an explanation of these colors/attributes. Scroll down to the SGR section.
-
-	Examples
-
-	Standard Colors:
-		// "panic:" with a red foreground then normal "rip"
-		color.Printf("%h[fgRed]panic:%r rip\n")
-
-		// "panic:" with a brightRed background then normal "rip"
-		color.Printf("%h[bgBrightRed]panic:%r rip\n")
-
-	Attributes:
-		// bold "panic:" then normal "rip"
-		color.Printf("%h[bold]panic:%r rip\n")
-
-		// underlined "panic:" with then normal "rip"
-		color.Printf("%h[underline]panic:%r rip\n")
-
-	256 Colors:
-		// "panic:" with a green foreground then normal "rip"
-		color.Printf("%h[fg2]panic:%r rip\n")
-
-		// "panic:" with a bright green background then normal "rip"
-		color.Printf("%h[bg10]panic:%r rip\n")
-
-	Mixing Colors and Attributes:
-		// bolded "panic:" with a green foreground then normal "rip"
-		color.Printf("%h[fgGreen+bold]panic:%r rip\n")
-
-		// underlined "panic:" with a bright black background then normal "rip"
-		color.Printf("%h[bg8+underline]panic:%r rip\n")
-
-	How does reset behave?
-		// bolded "panic:" with a blue foreground
-		// then bolded "rip" with a green foreground and bright black background
-		color.Printf("%h[fgBlue+bold]panic: %h[bg8]rip\n")
-
-		// bolded "hi" with a green foreground and bright black background
-		fmt.Printf("hi")
-
-		// finally resets the highlighting
-		color.Printf("%r")
-
-	log.Logger wrapper:
-		logger := color.NewLogger(os.Stderr, "", 0)
-
-		// prints hi in red
-		logger.Printf("%h[fgRed]hi%r")
-
-		// prints hi in red and then exits
-		logger.Fatalf("%h[fgRed]hi%r")
 */
 package color

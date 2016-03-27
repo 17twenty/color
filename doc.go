38f06eff417bc7e8ce68a6b33105956aa9f6eb13
Anmol Sethi <anmol@aubble.com>
2016-03-27 14:26:14 -0400

    doc update

--- doc.go
+++ doc.go
@@ -6,36 +6,60 @@
 		%h[attrs]		uses the attrs to highlight the following text
 		%r			an abbreviation for %h[reset]
 	
-	attrs is a + separated list of Colors (fgRed) or Attributes (bold).
+	attrs is a + separated list of Colors (e.g. fgRed) or Attributes (e.g. bold).
 
 	Multiple highlight verbs do not reset preceeding verbs, they add onto them.
 	For example, if you set the foreground to green in the first verb, then set the background to red in the second, any text following the second will have a green foreground and a red background.
 	
 	Standard Colors:
-		// red "panic:" and then normal "rip"
+		// "panic:" with a red foreground then normal "rip"
 		color.Printf("%h[fgRed]panic:%r rip\n")
 
-		// "panic:" with brightRed background and then normal "rip"
+		// "panic:" with a brightRed background then normal "rip"
 		color.Printf("%h[bgBrightRed]panic:%r rip\n")
 
 	Attributes:
-		// red "panic:" and then "rip" with a cyan background
-		color.Printf("%h[fg1]panic:%r %h[bg6]rip\n")
+		// bold "panic:" then normal "rip"
+		color.Printf("%h[bold]panic:%r rip\n")
+
+		// underlined "panic:" with then normal "rip"
+		color.Printf("%h[underline]panic:%r rip\n")
 
 	256 Colors:
-		// red "panic:" and then "rip" with a cyan background
-		color.Printf("%h[fg1]panic:%r %h[bg6]rip\n")
+		// "panic:" with a green foreground then normal "rip"
+		color.Printf("%h[fg2]panic:%r rip\n")
 
-	Mixing colors and attributes:
-		// bolded green "panic:" and then underlined "rip" with bright black background
-		color.Printf("%h[fgGreen+bold]panic:%r %h[bg8+underline]rip%r\n")
+		// "panic:" with a bright green background then normal "rip"
+		color.Printf("%h[bg10]panic:%r rip\n")
 
-	Reset behavior:
-		// bolded green "panic:" and then bolded green "rip" with bright black background
-		color.Printf("%h[fgGreen+bold]panic: %h[bg8]rip\n")
-		// bolded green "hi" with bright black background
+	Mixing Colors and Attributes:
+		// bolded "panic:" with a green foreground then normal "rip"
+		color.Printf("%h[fgGreen+bold]panic:%r rip\n")
+
+		// underlined "panic:" with a bright black background then normal "rip"
+		color.Printf("%h[bg8+underline]panic:%r rip\n")
+
+	How does reset behave?
+		// bolded "panic:" with a blue foreground
+		// then bolded "rip" with a green foreground and bright black background
+		color.Printf("%h[fgBlue+bold]panic: %h[bg8]rip\n")
+
+		// bolded "hi" with a green foreground and bright black background
 		fmt.Printf("hi")
+
 		// finally resets the highlighting
 		color.Printf("%r")
+
+	log.Logger wrapper:
+		logger := color.NewLogger(os.Stderr, "", 0)
+
+		// prints hi in red
+		logger.Printf("%h[fgRed]hi%r")
+
+		// prints hi in red and then exits
+		logger.Fatalf("%h[fgRed]hi%r")
+
+	Reference:
+		A syntax reference is [included](REFERENCE.md)
 */
 package color

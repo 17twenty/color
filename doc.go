ba7b2ee4a0d6e604cdc91b1abead20e6b324a659
Anmol Sethi <anmol@aubble.com>
2016-03-27 16:39:55 -0400

    include syntax reference in godoc

--- doc.go
+++ doc.go
@@ -63,5 +63,70 @@
 		// prints hi in red and then exits
 		logger.Fatalf("%h[fgRed]hi%r")
 
+	Syntax Reference
+		16 Foreground Colors:
+			%h[fgBlack]
+			%h[fgRed]
+			%h[fgGreen]
+			%h[fgYellow]
+			%h[fgBlue]
+			%h[fgMagenta]
+			%h[fgCyan]
+			%h[fgWhite]
+			%h[fgDefault]
+			%h[fgBrighBlack]
+			%h[fgBrightRed]
+			%h[fgBrightGreen]
+			%h[fgBrightYellow]
+			%h[fgBrightBlue]
+			%h[fgBrightMagenta]
+			%h[fgBrightCyan]
+			%h[fgBrightWhite]
+
+		16 Background Colors:
+			%h[bgBlack]
+			%h[bgRed]
+			%h[bgGreen]
+			%h[bgYellow]
+			%h[bgBlue]
+			%h[bgMagenta]
+			%h[bgCyan]
+			%h[bgWhite]
+			%h[bgDefault]
+			%h[bgBrighBlack]
+			%h[bgBrightRed]
+			%h[bgBrightGreen]
+			%h[bgBrightYellow]
+			%h[bgBrightBlue]
+			%h[bgBrightMagenta]
+			%h[bgBrightCyan]
+			%h[bgBrightWhite]
+
+		256 Colors:
+			%h[fgxxx]
+			%h[bgxxx]
+		Where xxx is any number from 0-255
+
+		Attributes:
+			%h[reset] or the %r verb
+			%h[bold]
+			%h[faint]
+			%h[italic]
+			%h[underline]
+			%h[blink]
+			%h[inverse]
+			%h[invisible]
+			%h[crossedOut]
+			%h[doubleUnderline]
+			%h[normal]
+			%h[notItalic]
+			%h[notUnderlined]
+			%h[steady]
+			%h[positive]
+			%h[visible]
+			%h[notCrossedOut]
+
+		Mixing:
+			%h[fgBlue+bgRed+bold]
 */
 package color

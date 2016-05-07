3ba1db01ec2bb00590daa8110b3621b498553ea2
Anmol Sethi <anmol@aubble.com>
2016-05-06 20:54:20 -0400

    removed IsTerminalWriter

--- terminal.go
+++ terminal.go
@@ -11,11 +11,3 @@ import (
 func IsTerminal(f *os.File) bool {
 	return terminal.IsTerminal(int(f.Fd()))
 }
-
-// IsTerminalWriter returns true if w is a terminal and false otherwise.
-func IsTerminalWriter(w io.Writer) bool {
-	if f, ok := w.(*os.File); ok {
-		return terminal.IsTerminal(int(f.Fd()))
-	}
-	return false
-}

9555f1ea6de025c84e717442525e395a1ef48959
Anmol Sethi <anmol@aubble.com>
2016-05-06 22:11:43 -0400

    more clean prepare section

--- doc.go
+++ doc.go
@@ -26,9 +26,9 @@ Everything else is handled by the fmt package. You should read its documentation
 
 Preparing Strings
 
-While this package is heavily optimized, processing the highlighting verbs is still very expensive. Thus, it makes more sense to process the verbs once and store the results in a structure.
+While this package is heavily optimized, processing the highlighting verbs is still very expensive. Thus, it makes more sense to process the verbs once and then store the results.
 
-This structure is named Format, and it holds the colored and stripped versions of the base format string. In the colored string, the highlight verbs are replaced with their control sequences. In contrast, the highlight verbs are completely removed in the stripped string. If color output is enabled, the colored string is used, but if color output is disabled, then the stripped string is used.
+A structure named Format is used for storage. It holds the colored and stripped versions of the base format string. In the colored string, the highlight verbs are replaced with their control sequences. In contrast, the highlight verbs are completely removed in the stripped string. Why both? If color output is enabled, the colored string is used, but if color output is disabled, then the stripped string is used.
 
 Use the Prepare function to create the Format structures and then use the Printfp like functions to print them. See Prepare below for an example.
 

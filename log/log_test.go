0e69986d9631b1fb8fc0ae6b2d2fe4e03376b237
Anmol Sethi <anmol@aubble.com>
2016-05-18 03:57:36 -0400

    refactored tests

--- log/log_test.go
+++ log/log_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/nhooyr/color"
 )
 
-func TestPrintf(t *testing.T) {
+func TestPrintfAndPrintfp(t *testing.T) {
 	t.Parallel()
 	var b bytes.Buffer
 	l := New(&b, true)
@@ -21,35 +21,24 @@ func TestPrintf(t *testing.T) {
 		t.Errorf("Expected %q but result was %q", exp, b.String())
 	}
 	b.Reset()
+	l.Printf(f, f2)
+	if b.String() != exp {
+		t.Errorf("Expected %q but result was %q", exp, b.String())
+	}
 	l.SetColor(false)
 	exp = fmt.Sprintf(f.Get(false), f2.Get(false)) + "\n"
 	l.Printf(s, f2)
 	if b.String() != exp {
 		t.Errorf("Expected %q but result was %q", exp, b.String())
 	}
-}
-
-func TestPrintfp(t *testing.T) {
-	t.Parallel()
-	var b bytes.Buffer
-	l := New(&b, true)
-	f := color.Prepare("%h[fgBlue]bar:%r %s\n")
-	f2 := color.Prepare("%h[fgWhite]bar")
-	exp := fmt.Sprintf(f.Get(true), f2.Get(true))
-	l.Printfp(f, f2)
-	if b.String() != exp {
-		t.Errorf("Expected %q but result was %q", exp, b.String())
-	}
 	b.Reset()
-	l.SetColor(false)
-	exp = fmt.Sprintf(f.Get(false), f2.Get(false))
-	l.Printfp(f, f2)
+	l.Printf(f, f2)
 	if b.String() != exp {
 		t.Errorf("Expected %q but result was %q", exp, b.String())
 	}
 }
 
-func TestPrint(t *testing.T) {
+func TestPrintAndPrintln(t *testing.T) {
 	t.Parallel()
 	var b bytes.Buffer
 	l := New(&b, true)
@@ -60,27 +49,19 @@ func TestPrint(t *testing.T) {
 		t.Errorf("Expected %q but result was %q", exp, b.String())
 	}
 	b.Reset()
+	exp = f.Get(true) + " foo\n"
+	l.Println(f, "foo")
+	if b.String() != exp {
+		t.Errorf("Expected %q but result was %q", exp, b.String())
+	}
+	b.Reset()
 	exp = f.Get(false) + "foo\n"
 	l.SetColor(false)
 	l.Print(f, "foo")
 	if b.String() != exp {
 		t.Errorf("Expected %q but result was %q", exp, b.String())
 	}
-}
-
-func TestPrintln(t *testing.T) {
-	t.Parallel()
-	var b bytes.Buffer
-	l := New(&b, true)
-	f := color.Prepare("%h[fgWhite]bar")
-	exp := f.Get(true) + " foo\n"
-	l.Println(f, "foo")
-	if b.String() != exp {
-		t.Errorf("Expected %q but result was %q", exp, b.String())
-	}
-	b.Reset()
 	exp = f.Get(false) + " foo\n"
-	l.SetColor(false)
 	l.Println(f, "foo")
 	if b.String() != exp {
 		t.Errorf("Expected %q but result was %q", exp, b.String())

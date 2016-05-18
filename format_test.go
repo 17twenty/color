17944497d94b6a558cd7c055bdb5d0b2151ddba3
Anmol Sethi <anmol@aubble.com>
2016-05-18 02:01:35 -0400

    format tests

---
+++ format_test.go
@@ -0,0 +1,56 @@
+package color
+
+import (
+	"testing"
+
+	"github.com/nhooyr/terminfo/caps"
+)
+
+func TestPrepare(t *testing.T) {
+	f := Prepare("%h[fgBlue]foo")
+	exp := ti.Color(caps.Blue, -1) + "foo"
+	r := f.Get(true)
+	if exp != r {
+		t.Errorf("Expected %q but result was %q", exp, r)
+	}
+	exp = "foo"
+	r = f.Get(false)
+	if exp != r {
+		t.Errorf("Expected %q but result was %q", exp, r)
+	}
+}
+
+func TestEprintf(t *testing.T) {
+	f := Prepare("%h[fgRed]panic: %s: %s").Eprintfp("bar", Prepare("%h[fgGreen]rip"))
+	exp := ti.Color(caps.Red, -1) + "panic: bar: " + ti.Color(caps.Green, -1) + "rip"
+	r := f.Get(true)
+	if exp != r {
+		t.Errorf("Expected %q but result was %q", exp, r)
+	}
+	exp = "panic: bar: rip"
+	r = f.Get(false)
+	if exp != r {
+		t.Errorf("Expected %q but result was %q", exp, r)
+	}
+}
+
+func TestExpandFormats(t *testing.T) {
+	a := [3]interface{}{
+		Prepare("%h[bgMagenta]foo"),
+		"bar",
+		3,
+	}
+	exp := a
+	exp[0]= ti.Color(-1, caps.Magenta) + "foo"
+	r := a
+	ExpandFormats(true, r[:])
+	if exp != r {
+		t.Errorf("Expected %q but result was %q", exp, r)
+	}
+	exp[0] = "foo"
+	r[0] = a[0]
+	ExpandFormats(false, r[:])
+	if exp != r {
+		t.Errorf("Expected %q but result was %q", exp, r)
+	}
+}
